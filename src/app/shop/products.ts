1d0c2b5a461c26511dad0198feed0e366e44cd79
Czerwcuu <czerwiecr1999@gmail.com>
2022-02-10 19:51:07 +0100

    shop frontend

--- src/app/shop/products.ts
+++ src/app/shop/products.ts
@@ -7,27 +7,27 @@ export interface Product {
     description: string;
     img: string
   }
-  
+
   export const products = [
     {
       id: 1,
       name: 'Maseczka',
       price: 799,
       description: 'Maska antywirusowa przeznaczona do jednorazowego użytku stanowi higieniczną i bezpieczną alternatywę dla maseczek wielorazowych, które wykonane są z tkaniny.',
-      img: "https://i.imgur.com/OfPX880.png"
+      img: "https://i.imgur.com/BIc6zSl.png"
     },
     {
       id: 2,
       name: 'Przyłbica',
       price: 699,
       description: 'Łączy w sobie zarówno wygodę maseczki jak i dzięki przeźroczystości spełnia funkcję przyłbicy.',
-      img: "https://i.imgur.com/0m2l1jp.jpeg"
+      img: "https://i.imgur.com/tK4Z5ej.png"
     },
     {
       id: 3,
       name: 'Rękawiczki lateksowe',
       price: 299,
       description: 'Rękawiczki lateksowe są wykonane z wysokiej jakości kauczuku naturalnego. Doskonale łączą w sobie właściwości ochronne, takie jak barierowość i wytrzymałość. Cechują się wysoką elastycznością, dlatego dobrze dopasowują się do dłoni i gwarantują tym samym precyzję i swobodę ruchów.',
-      img: "https://i.imgur.com/BMIMcJI.jpeg"
+      img: "https://i.imgur.com/ISOYeOy.png"
     }
   ];
